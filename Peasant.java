ce5605fffce5b003c7e1bc162953bd26096bd583
Bor P <golovanovaea.7@gmail.com>
2023-04-23 14:36:38 +0300

    peasant

--- Peasant.java
+++ Peasant.java
@@ -5,7 +5,11 @@ public class Peasant extends Unit_peasant_monk {
 
     Peasant(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, float thePowerOfTheEarth, int powerOfKind){
 
-        super("name", "hoe", 100, 10, 10, 1, 100, 10, 0);
+        super(name, "hoe", 100, 10, 10, 1, 100, 10, 0);
     }
 
+    @Override
+    public String toString(){
+        return name;
+    }
 }
