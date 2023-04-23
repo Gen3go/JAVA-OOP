3e0bbef3b588ec06d8f2a363b7ac1ed01a3ec5f6
Bor P <golovanovaea.7@gmail.com>
2023-04-23 14:26:21 +0300

    witch

---
+++ Witch.java
@@ -0,0 +1,6 @@
+public class Witch extends Unit_robber_witch{
+    Witch(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, float magic){
+        super("name", "sorcerer's staff", 100, 100, 80, 50, 100, 100);
+    }
+    
+}
