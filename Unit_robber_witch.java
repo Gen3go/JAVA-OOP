39f4b50e537efddbf5b76ea8019e83e9a7b42dd5
Bor P <golovanovaea.7@gmail.com>
2023-04-23 14:18:09 +0300

    4й абстрактный класс

---
+++ Unit_robber_witch.java
@@ -0,0 +1,16 @@
+public class Unit_robber_witch extends Unit {
+
+    protected float magic;
+
+    Unit_robber_witch(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, float magic){
+        super(name, usedItem, health, speed, inflictedIiron, damage, money);
+        this.magic = magic;
+    }
+
+    
+    void disappearIntoTheCrowd(){
+
+    }
+
+
+}
