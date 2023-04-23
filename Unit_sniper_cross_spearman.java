4e020403a3a1d04fbf1a6f4da410d6e0b7aeaaa0
Bor P <golovanovaea.7@gmail.com>
2023-04-23 14:17:23 +0300

    3й абстрактный класс

---
+++ Unit_sniper_cross_spearman.java
@@ -0,0 +1,17 @@
+public class Unit_sniper_cross_spearman extends Unit {
+
+        protected int accuracy;
+    
+        public Unit_sniper_cross_spearman(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, int accuracy) {
+            super(name, usedItem, health, speed, inflictedIiron, damage, money);
+            this.accuracy = accuracy;
+    
+        }
+
+        void restoreHealthFromMoney(){
+
+        }
+        void superVision(){
+
+        }
+}
