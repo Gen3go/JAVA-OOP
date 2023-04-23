9962269ce760296b27691279ce2d6b7df144a19f
Bor P <golovanovaea.7@gmail.com>
2023-04-23 12:38:08 +0300

    1й Абстрактный класс

---
+++ Unit.java
@@ -0,0 +1,26 @@
+public class Unit {
+    protected String name, usedItem;
+    protected int health, speed;
+    protected float inflictedIiron;
+    protected int damage;
+    protected int money;
+
+    public Unit(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money) {
+        this.name = name;
+        this.usedItem = usedItem;
+        this.health = health;
+        this.speed = speed;
+        this.inflictedIiron = inflictedIiron;
+        this.damage = damage;
+        this.money = money;
+
+    }
+
+
+    void attack(){
+
+
+    }
+    
+}
+
