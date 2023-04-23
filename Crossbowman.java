42e778ea3d0d2eadeaff7ff02aadffc6acf5e7ae
Bor P <golovanovaea.7@gmail.com>
2023-04-23 14:23:50 +0300

    crossbowman

---
+++ Crossbowman.java
@@ -0,0 +1,5 @@
+public class Crossbowman extends Unit_sniper_cross_spearman{
+    Crossbowman(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, int accuracy){
+        super("name", "crossbow", 100, 40, 40, 1, 100, 20);
+    }
+}
