eb27053b646916ec84fba2282133f225351d5d49
Bor P <golovanovaea.7@gmail.com>
2023-04-23 14:22:41 +0300

    monk

---
+++ Monk.java
@@ -0,0 +1,13 @@
+public class Monk extends Unit_peasant_monk {
+
+
+    Monk(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, float thePowerOfTheEarth, int powerOfKind){
+
+        super("name", "snag", 100, 10, 5, 1, 100, 10, 10);
+
+    }
+
+    void takePowerOfKind(){
+
+    }
+}
