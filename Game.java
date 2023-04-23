e24291ba7bd5ec5eacab9cd2ca96211c70e4f436
Bor P <golovanovaea.7@gmail.com>
2023-04-23 14:35:46 +0300

    game

---
+++ Game.java
@@ -0,0 +1,6 @@
+public class Game {
+    public static void main(String[] args) {
+        Peasant peasant = new Peasant("Авоська", null, 0, 0, 0, 0, 0, 0, 0);
+        System.out.println(peasant);
+    }
+}
