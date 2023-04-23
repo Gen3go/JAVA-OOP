5a0cff80853b7a949f8efa63662b3be3eeeb09a7
Bor P <golovanovaea.7@gmail.com>
2023-04-23 14:25:52 +0300

    spearmen

---
+++ Spearmen.java
@@ -0,0 +1,9 @@
+public class Spearmen extends Unit_sniper_cross_spearman{
+    Spearmen(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, int accuracy){
+        super("name", "spear", 100, 30, 50, 1, 100, 10);
+    }
+
+    void restoreSpearFromMoney(){
+        
+    }
+}
