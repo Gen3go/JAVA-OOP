5dc2042a3a8a8ed3aa6c8b95f8a33ec1d7135892
Bor P <golovanovaea.7@gmail.com>
2023-04-23 14:23:07 +0300

    robber

---
+++ Robber.java
@@ -0,0 +1,14 @@
+class Robber extends Unit_robber_witch{
+
+    Robber(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, float magic){
+        super("name", "knife", 100, 50, 70, 2, 100, 0);
+    }
+    void stealThings(){
+
+    }
+    void payThing(){
+
+    }
+    
+
+}
