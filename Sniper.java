d42d43012e07c388a4bc1a1e2fb41e5886c2e23f
Bor P <golovanovaea.7@gmail.com>
2023-04-23 14:24:22 +0300

    sniper

---
+++ Sniper.java
@@ -0,0 +1,6 @@
+public class Sniper extends Unit_sniper_cross_spearman{
+
+    Sniper(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, int accuracy){
+        super("name", "rifle", 100, 50, 50, 1, 100, 30);
+    }
+}
