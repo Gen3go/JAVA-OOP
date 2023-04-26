2565a66c673f20cc42007d9af16b8644b879bc90
Bor P <golovanovaea.7@gmail.com>
2023-04-26 18:13:55 +0300

    Добавлены методы

--- Monk.java
+++ Monk.java
@@ -1,13 +1,36 @@
 public class Monk extends Unit_peasant_monk {
 
 
+    public static char[] getInfo;
+
     Monk(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, float thePowerOfTheEarth, int powerOfKind){
 
-        super("name", "snag", 100, 10, 5, 1, 100, 10, 10);
+        super("Neof", "snag", 100, 10, 5, 1, 100, 10, 10);
 
     }
 
     void takePowerOfKind(){
 
     }
+
+    @Override
+    public String toString(){
+        return "Монах";
+    }
+
+    @Override
+    public int getHealth() {
+        return health;
+    }
+
+    @Override
+    public String getInfo() {
+        return name + " Используемый предмет: " + " Здоровье: "+ health +
+        " Скорость: " + speed + " Наносимый урон: " + inflictedIiron + 
+        " Урон: " + damage + " Деньги: " + " Сила земли: " + thePowerOfTheEarth + 
+        " Сила доброты: " + powerOfKind;
+    }
+
+    
+
 }
