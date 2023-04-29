73c36f1917d38ba70f795b6fdb920c242403e9e0
Bor P <golovanovaea.7@gmail.com>
2023-04-29 14:48:52 +0300

    Корректировка

--- Unit_robber_witch.java
+++ Unit_robber_witch.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 public class Unit_robber_witch extends Unit {
 
     protected float magic;
 
-    Unit_robber_witch(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, float magic){
-        super(name, usedItem, health, speed, inflictedIiron, damage, money);
+    Unit_robber_witch(ArrayList<Unit> team, String name, String usedItem, int health, int intiative, float inflictedIiron, int damage, int money, float magic){
+        super(team, name, usedItem, health, intiative, inflictedIiron, damage, money);
         this.magic = magic;
     }
 
@@ -12,5 +14,17 @@ public class Unit_robber_witch extends Unit {
 
     }
 
+    @Override
+    public void step() {
+        int index_damaged = 0;
+        for (int i = 0; i < team.size(); i++) {
+            for (int j = i+1; j < team.size()-1; j++) {
+                if(team.get(i).health/team.get(i).maxHealth < team.get(j).health/team.get(j).maxHealth){
+                    index_damaged = i;
+                }
+            }
+        }
+        
+    }
 
 }
