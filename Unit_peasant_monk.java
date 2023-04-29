5a3741aa86cf99fdb76094b5a304cd4d006d7ddd
Bor P <golovanovaea.7@gmail.com>
2023-04-29 14:48:30 +0300

    Корректировка

--- Unit_peasant_monk.java
+++ Unit_peasant_monk.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
+
 public class Unit_peasant_monk extends Unit{
 
     protected float thePowerOfTheEarth;
     protected int powerOfKind;
 
 
-    public Unit_peasant_monk(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, float thePowerOfTheEarth, int powerOfKind) {
-        super(name, usedItem, health, speed, inflictedIiron, damage, money);
+    public Unit_peasant_monk(ArrayList<Unit> team, String name, String usedItem, int health, int intiative, float inflictedIiron, int damage, int money, float thePowerOfTheEarth, int powerOfKind) {
+        super(team, name, usedItem, health, intiative, inflictedIiron, damage, money);
         this.thePowerOfTheEarth = thePowerOfTheEarth;
         this.powerOfKind = powerOfKind;
 
