6618a7a85e508145bcac6c0e8b11b4254d804ff5
Bor P <golovanovaea.7@gmail.com>
2023-04-29 14:49:24 +0300

    Корректировка

--- Unit_sniper_cross_spearman.java
+++ Unit_sniper_cross_spearman.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 public class Unit_sniper_cross_spearman extends Unit {
 
         protected int accuracy;
     
-        public Unit_sniper_cross_spearman(String name, String usedItem, int health, int speed, float inflictedIiron, int damage, int money, int accuracy) {
-            super(name, usedItem, health, speed, inflictedIiron, damage, money);
+        public Unit_sniper_cross_spearman(ArrayList<Unit> team, String name, String usedItem, int health, int intiative, float inflictedIiron, int damage, int money, int accuracy) {
+            super(team, name, usedItem, health, intiative, inflictedIiron, damage, money);
             this.accuracy = accuracy;
     
         }
