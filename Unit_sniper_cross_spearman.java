9eab94d3a35b3bc6f3cb48fdb6fbf8669d217905
Bor P <golovanovaea.7@gmail.com>
2023-05-05 17:22:48 +0300

    Корректировка класса

--- Unit_sniper_cross_spearman.java
+++ Unit_sniper_cross_spearman.java
@@ -4,8 +4,8 @@ public class Unit_sniper_cross_spearman extends Unit {
 
         protected int accuracy;
     
-        public Unit_sniper_cross_spearman(ArrayList<Unit> team, String name, String usedItem, int health, int intiative, float inflictedIiron, int damage, int money, int accuracy) {
-            super(team, name, usedItem, health, intiative, inflictedIiron, damage, money);
+        public Unit_sniper_cross_spearman(ArrayList<Unit> team, float x, float y) {
+            super(team, name, usedItem, health, intiative, inflictedIiron, damage, money, x, y);
             this.accuracy = accuracy;
     
         }
