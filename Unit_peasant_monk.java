940909e68ba408e28b2035bdc4344a64bf2026ad
Bor P <golovanovaea.7@gmail.com>
2023-05-05 17:21:34 +0300

    Корректировка класса

--- Unit_peasant_monk.java
+++ Unit_peasant_monk.java
@@ -6,8 +6,8 @@ public class Unit_peasant_monk extends Unit{
     protected int powerOfKind;
 
 
-    public Unit_peasant_monk(ArrayList<Unit> team, String name, String usedItem, int health, int money, int intiative, float inflictedIiron, int damage, float thePowerOfTheEarth, int powerOfKind) {
-        super(team, name, usedItem, health, money, intiative, inflictedIiron, damage);
+    public Unit_peasant_monk(ArrayList<Unit> team, String name, float x, float y) {
+        super(team, name, usedItem, health, money, intiative, inflictedIiron, damage, x, y);
         this.thePowerOfTheEarth = thePowerOfTheEarth;
         this.powerOfKind = powerOfKind;
 
